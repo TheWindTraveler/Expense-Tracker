2b1f0749c2f14056885ab7728146ccd7bd5b529d
Begotten-Traveler <solomondev002@gmail.com>
2025-07-08 07:34:32 +0100

    add: javascript

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Yo I'm linked");
+const expenseForm = document.getElementById("expense-form");
+const expenseInput = document.getElementById("expense-input");
+const amountInput = document.getElementById("amount-input");
+const categoryInput = document.getElementById("category-input");
+const transactionList = document.getElementById("transaction-list");
+const totalExpense = document.getElementById("total-expense");
+const totalIncome = document.getElementById("balance");
