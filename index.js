81c49462f15d50c2ef24463cddc6199211b108fd
Begotten-Traveler <solomondev002@gmail.com>
2025-07-09 17:09:28 +0100

    chore: bugs fixed

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const balance = document.getElementById("balance");
 const incomeForm = document.getElementById("income-form");
 const incomeDescriptionInput = document.getElementById("income-description");
 const incomeAmountInput = document.getElementById("income-amount");
+const clearAllInput = document.getElementById("clear-all");
 
 incomeForm.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -90,8 +91,19 @@ function updateSummary() {
     balance.textContent = (totalIncomes - totalExpenses).toFixed(2);
 };
 
-function clearInputs() {
+clearAllInput.addEventListener('click', function() {
+    // Clear all input fields
     expenseInput.value = '';
     amountInput.value = '';
     categoryInput.value = '';
-}
+    incomeDescriptionInput.value = '';
+    incomeAmountInput.value = '';
+
+    // Remove all rows from transaction table
+    transactionList.innerHTML = '';
+
+    // Reset summary
+    totalExpense.textContent = '0.00';
+    totalIncome.textContent = '0.00';
+    balance.textContent = '0.00';
+});
